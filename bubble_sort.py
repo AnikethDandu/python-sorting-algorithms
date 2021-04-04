ba09f1ca0e7e826303abbafc47bda35091536d12
AnikethDandu <aniketh.dandu@icloud.com>
2021-04-04 17:46:22 -0400

    Refactor parameter name and remove print test

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,10 +1,7 @@
-def bubble_sort(array):
-    n = len(array)
+def bubble_sort(arr):
+    n = len(arr)
     for i in range(n-1):
         for j in range(0, n-i-1):
-            if array[j] > array[j+1]:
-                array[j], array[j+1] = array[j+1], array[j]
-    return array
-
-
-print(bubble_sort([7, 2, 4, 5, 9, 1, 4, 0, 2, 34, 5]))
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
