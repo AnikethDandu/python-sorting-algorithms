7857caac71c4f2abf976d23fac3909894ecb089a
AnikethDandu <aniketh.dandu@icloud.com>
2021-04-05 15:25:01 -0400

    Refactor Selection Sort method to use only minimum value index instead of both index and value

--- selection_sort.py
+++ selection_sort.py
@@ -1,14 +1,12 @@
 def selection_sort(arr):
     n = len(arr)
     for i in range(n - 1):
-        j_index = -1
-        min_num = arr[i]
+        min_index = i
         for j in range(i + 1, n):
-            if arr[j] < min_num:
-                min_num = arr[j]
-                j_index = j
-        if j_index > -1:
-            arr[i], arr[j_index] = arr[j_index], arr[i]
+            if arr[j] < arr[min_index]:
+                min_index = j
+        if min_index > i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 
 
