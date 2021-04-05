668cf7db32a5ff439d4e98c0165a97321db40e0e
AnikethDandu <aniketh.dandu@icloud.com>
2021-04-05 15:17:34 -0400

    Complete Selection Sort method

--- selection_sort.py
+++ selection_sort.py
@@ -1,2 +1,15 @@
 def selection_sort(arr):
-    return
+    n = len(arr)
+    for i in range(n - 1):
+        j_index = -1
+        min_num = arr[i]
+        for j in range(i + 1, n):
+            if arr[j] < min_num:
+                min_num = arr[j]
+                j_index = j
+        if j_index > -1:
+            arr[i], arr[j_index] = arr[j_index], arr[i]
+    return arr
+
+
+print(selection_sort([3, 1, 2]))
