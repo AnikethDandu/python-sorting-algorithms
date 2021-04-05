2268037436144ae09dd6fe6506d513a3ee3f7c15
AnikethDandu <aniketh.dandu@icloud.com>
2021-04-05 14:49:24 -0400

    Add reST documentation for bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,7 +1,27 @@
-def bubble_sort(arr):
+"""
+Bubble Sort
+
+This module defines the bubble sort method
+
+FUNCTIONS:
+    - bubble_sort(arr) -> list
+"""
+
+__docformat__ = 'reStructuredText'
+
+
+def bubble_sort(arr: list) -> list:
+    """
+    Returns a sorted array using bubble sort
+
+    :param arr: Array to be sorted
+    :type arr: list
+    :return: Sorted array
+    :rtype: list
+    """
     n = len(arr)
-    for i in range(n-1):
-        for j in range(0, n-i-1):
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
