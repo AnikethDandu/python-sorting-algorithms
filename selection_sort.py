7b38fede9f53f65400bcb63ced42789df6138711
AnikethDandu <aniketh.dandu@icloud.com>
2021-04-05 15:36:26 -0400

    Add documentation in selection_sort.py and remove print test statement

--- selection_sort.py
+++ selection_sort.py
@@ -1,4 +1,26 @@
-def selection_sort(arr):
+"""
+Selection Sort
+
+This module defines the selection sort method
+
+FUNCTIONS:
+    - selection_sort(arr) -> list
+"""
+
+__docformat__ = 'reStructuredText'
+
+
+def selection_sort(arr: list) -> list:
+    """
+    Returns a sorted array using selection sort
+
+    Time complexity: O(n^2)
+
+    :param arr: Array to be sorted
+    :type arr: list
+    :return: Sorted array
+    :rtype: list
+    """
     n = len(arr)
     for i in range(n - 1):
         min_index = i
@@ -7,7 +29,5 @@ def selection_sort(arr):
                 min_index = j
         if min_index > i:
             arr[i], arr[min_index] = arr[min_index], arr[i]
+        print(arr)
     return arr
-
-
-print(selection_sort([3, 1, 2]))
