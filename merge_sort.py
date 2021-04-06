9c905b2cfeb14a285cbbb2957eb916951091b2d8
AnikethDandu <aniketh.dandu@icloud.com>
2021-04-06 12:04:32 -0400

    Add merge method to merge_sort.py to merge two sorted arrays

--- merge_sort.py
+++ merge_sort.py
@@ -7,4 +7,24 @@ def merge_sort(arr):
         merge_sort(right_arr)
 
 
-merge_sort([0, 1, 2, 3, 4, 5])
+def merge(arr_one, arr_two):
+    arr_temp = [0] * (len(arr_one) + len(arr_two))
+    i = j = k = 0
+    while i < len(arr_one) and j < len(arr_two):
+        if arr_one[i] < arr_two[j]:
+            arr_temp[k] = arr_one[i]
+            i += 1
+        else:
+            arr_temp[k] = arr_two[j]
+            j += 1
+        k += 1
+    if i < len(arr_one):
+        for x in range(i, len(arr_one)):
+            arr_temp[i+x] = arr_one[x]
+    if j < len(arr_two):
+        for x in range(j, len(arr_two)):
+            arr_temp[i+x] = arr_two[x]
+    return arr_temp
+
+
+print(merge([1, 2, 3, 5], [4, 7, 9]))
