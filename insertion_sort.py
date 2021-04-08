9f1a3463959bf1aa60b545d9d5769d9f89e89517
AnikethDandu <aniketh.dandu@icloud.com>
2021-04-08 11:59:13 -0400

    Add reST docstring and code comments to insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,7 +1,37 @@
-def insertion_sort(arr):
+"""
+Insertion Sort
+
+This module defines the Insertion Sort function
+
+FUNCTIONS:
+    - insertion_sort(arr) -> list
+"""
+
+__docformat__ = 'reStructuredText'
+
+
+def insertion_sort(arr: list) -> list:
+    """
+    Performs an Insertion Sort on unsorted array and returns sorted array
+
+    Time complexity: O(n^2)
+
+    :param arr: Unsorted array
+    :type arr: list
+    :return: Returns sorted array
+    :rtype: list
+    """
+
+    # n: length of array
+    # i: counter variable for iterating through array elements
+    # j: counter variable for comparing element to previous element
+
+    # Loop through array starting at index 1
+    # Compare each element to previous element
+    # If current element smaller, swap and repeat until comparison fails
     n = len(arr)
     for i in range(1, n):
-        for j in range(i-1, -1, -1):
+        for j in range(i - 1, -1, -1):
             if arr[i] < arr[j]:
                 arr[i], arr[j] = arr[j], arr[i]
                 i -= 1
