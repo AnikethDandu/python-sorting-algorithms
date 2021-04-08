b1cdebce8584c414b1feba5c6efcc4e1c98a1845
AnikethDandu <aniketh.dandu@icloud.com>
2021-04-08 11:55:00 -0400

    Remove Insertion Sort counter variable and add break once comparison to adjacent element fails

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,12 +1,10 @@
 def insertion_sort(arr):
     n = len(arr)
     for i in range(1, n):
-        count = 0
         for j in range(i-1, -1, -1):
-            if arr[i-count] < arr[j]:
-                arr[i-count], arr[j] = arr[j], arr[i-count]
-                count += 1
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+                i -= 1
+            else:
+                break
     return arr
-
-
-print(insertion_sort([5, 4, 3, 2, 1, 0]))
