98b9d7a1ecdcf93a99ea947b70103282ed3f32bc
AnikethDandu <aniketh.dandu@icloud.com>
2021-04-08 10:41:01 -0400

    Complete Insertion Sort function

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,1 +1,12 @@
-A
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        count = 0
+        for j in range(i-1, -1, -1):
+            if arr[i-count] < arr[j]:
+                arr[i-count], arr[j] = arr[j], arr[i-count]
+                count += 1
+    return arr
+
+
+print(insertion_sort([5, 4, 3, 2, 1, 0]))
