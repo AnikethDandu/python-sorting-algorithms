b1a8a5d045deffa64d201fbe044cd65d86a0c91a
AnikethDandu <aniketh.dandu@icloud.com>
2021-04-17 18:28:23 -0400

    Remove array_visualization.py

--- Visualization/array_visualization.py
+++
@@ -1,7 +0,0 @@
-from bubble_sort import bubble_sort
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-
-figure = plt.figure(figsize=(5, 5))
-ax = plt.axes(xlim=(0, 10000), ylim=(0, 1000))
-plt.show()
